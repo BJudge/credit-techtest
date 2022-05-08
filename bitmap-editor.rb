ac2396391dcfd8467b666008d4611b2fd985187a
Bart Judge <BartJudge@gmail.com>
2022-05-08 17:17:27 +0100

    scaling complete + chmod implemented

--- bitmap-editor.rb
+++ bitmap-editor.rb
@@ -1,12 +1,14 @@
+#!/usr/bin/ruby
 require 'enumerator'
-# exit the program
 # how does the user input their commands - https://stackoverflow.com/questions/9853853/creating-interactive-ruby-console-application
-#should these be class methods or instance methods 
+ 
 
 class Canvas
     def I (m, n)
         # @canvas = Array.new(m) { Array.new(n , "O")}
-        @canvas= m.times.map { Array.new(n, "O")}
+        @canvas = new_grid(m,n)
+        @number_of_rows = m 
+        @number_of_columns = n 
     end
 
     def S  
@@ -45,13 +47,36 @@ class Canvas
         end
     end
 
-    def W(f)
-        # a 3x3 canvas scaled by 250% would result in a 7.5 X 7.5 canvas - this is not possible so scalling rounds up (ceil)
-        multiple = (f/100.0)
-        new_x = (@canvas.length * multiple).ceil()
-        new_y = (@canvas[0].length * multiple).ceil()
-       
-        
+    def W(factor)
+        # a 3x3 canvas scaled by 250% would result in a 7.5 X 7.5 canvas - this is not possible so scalling rounds down (floor)
+        multiple = (factor/100.0)
+        new_rows_length = (@number_of_rows * multiple).floor
+        new_columns_length = (@number_of_columns * multiple).floor
+        diff_columns = (new_columns_length - @number_of_columns).abs
+        diff_rows = (new_rows_length - @number_of_rows).abs
+        if (factor == 100)
+            p @canvas
+        end
+        if (factor > 100)
+            new_canvas = []
+            @canvas.each {|row|
+                new_row = row.clone
+                diff_columns.times {|i| new_row.unshift("O")} 
+                new_canvas.push(new_row)
+            }
+            diff_rows.times{|i| new_canvas.push(Array.new(new_columns_length, "O"))} 
+            puts new_canvas
+        end
+        if (factor < 100)
+            new_canvas = []
+            @canvas.each {|row|
+                new_row = row.clone
+                diff_columns.times {|i| new_row.shift()} 
+                new_canvas.push(new_row)
+            }
+            diff_rows.times{|i| new_canvas.shift()} 
+            puts new_canvas
+        end
     end
 
     def F(x, y, c)
@@ -83,7 +108,16 @@ class Canvas
         exit
     end
 
-    # private 
+    private 
+
+    def new_grid(m,n)
+        m.times.map { Array.new(n, "O")}
+    end
+
+    def coord_validation()
+        # do coordinate checks for all functions that take in an x and a y 
+    end
+
      def find_region(new_additions, r, z)
         if (new_additions.length == 0) 
             return r
@@ -109,9 +143,23 @@ class Canvas
             # coordinates is an arry of two elements 
             x = coordinates[0]
             y = coordinates[1]
+            # check to see if neighbours are outside the grid
+            neighbours = []
+            if (x-1 > 0)
+                neighbours.push([x-1,y])
+            end
+            if (x+1 <= @number_of_columns)
+                neighbours.push([x+1,y])
+            end
+            if(y-1 > 0)
+                neighbours.push([x,y-1])
+            end
+            if(y+1 <= @number_of_rows)
+                neighbours.push([x,y+1])
+            end
 
-            return [[x+1, y],[x-1,y],[x,y+1],[x, y-1]]
-     end
+            return neighbours
+    end
 
      def find_colour(coordinates)
         x = coordinates[0]
@@ -141,20 +189,10 @@ end
 
 
 new_canvas = Canvas.new()
-new_canvas.I(10,10)
-new_canvas.L(5, 5,"W")
-new_canvas.L(5, 6,"W")
-
-# new_canvas.V(3,2,8,"B")
-# new_canvas.H(1,4,3,"W")
+new_canvas.I(3,2)
 new_canvas.S()
-new_canvas.F(5,5,"G")
+puts ""
+new_canvas.W(75)
 puts ""
 new_canvas.S()
-p new_canvas.find_region([[5,5]],[[5,5]], "W")
-# new_canvas.W(350)
-# new_canvas.?()
-# p ARGV # this takes the arguments and puts them into an array.
-# argument = gets.chomp
-# array_argument = argument.to_a
-# puts array_argument
+
